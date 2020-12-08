b0fdf9e266db4e384d35f8b0214fe4529d19f4ff
qr0904414 <60110778+qr0904414@users.noreply.github.com>
2020-12-08 16:27:42 -0600

    Initial Upload
    First upload

---
+++ Current Weather/README.md
@@ -0,0 +1,30 @@
+# current_weather
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+### Created by
+## Quinn Ridings
+
+### Netlify link
+## 
+
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ Current Weather/src/App.vue
@@ -0,0 +1,36 @@
+<!-- Main data for the page that doesn't change -->
+  <template>
+    <div id="app">
+      <h1> OpenWeather's Current Weather </h1>
+      <WeatherForm />
+    </div>
+  </template>
+
+<!-- Script to import the WeatherForm and export this component -->
+  <script>
+    import WeatherForm from './components/WeatherForm.vue'
+
+    export default {
+      name: 'App',
+      components: {
+        WeatherForm
+      }
+    }
+  </script>
+
+<!-- Style that is not localized to this component -->
+  <style>
+    * {
+      background-color: rgb(158, 158, 158);
+      color: black;
+      padding: 0;
+      margin: 1%;
+    }
+    #app
+    {
+      text-align: center;
+    }
+    h1 {
+      padding-bottom: 10px;
+    }
+  </style>
